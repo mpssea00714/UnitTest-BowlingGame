38f07d745ec6c384378d376490dbb535e389c3c3
David <seasky00714@gmail.com>
2021-09-25 16:27:15 +0800

    3rd-test

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -4,15 +4,38 @@ namespace BowlingGame
 {
     public class Game
     {
-        private int totalScore;
+        private int[] rolls = new int[20];
+        private int currentRoll = 0;
+
+
         public void Roll(int pins)
         {
-            totalScore += pins;
+            rolls[currentRoll++] = pins;
         }
 
         public int Score()
         {
+            int totalScore = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsSpare(frameIndex))
+                {
+                    totalScore += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    totalScore += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
             return totalScore;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
         }
     }
 }
--- BowlingGameTest/GameTest.cs
+++ BowlingGameTest/GameTest.cs
@@ -41,12 +41,36 @@ namespace BowlingGameTest
             Assert.AreEqual(actual, expected);
         }
 
+        [TestMethod]
+        //測試玩家整局只有一次補中+一次計分格的分數
+        //共20次投球機會,其餘17次皆0分
+        public void TsetOneSpare()
+        {
+            int expected = 16;
+            int actual;
+
+            RollSpare();
+            g.Roll(3);
+            RollMany(17, 0);
+
+            actual = g.Score();
+
+            Assert.AreEqual(actual, expected);
+        }
+
+
         private void RollMany(int n, int pins)
         {
             for (int i = 0; i < n; i++)
             {
                 g.Roll(pins);
             }
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
         }
     }
 }
